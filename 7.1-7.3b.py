20704ad882d6a9a2108a54afa5cc3fbdc955d29e
Алексей Стебло <asteblo@gmail.com>
2019-09-15 04:34:24 +0300

    4 ночи =(

--- 7.1-7.3b.py
+++ 7.1-7.3b.py
@@ -1,17 +1,17 @@
-# from sys import argv
+from sys import argv
 
-# f = open('7.1.txt')
-# ospf_route = f.read()
-# ospf_route = ospf_route.replace('O','OSPF').replace('[', '').replace(']', '').replace(',', '')
-# RESULT = ospf_route.split(' ')
-# print('Protocol:             ', RESULT[0])
-# print('Prefix:               ', RESULT[1])
-# print('AD/Metric:            ', RESULT[2])
-# print('Next-Hop:             ', RESULT[4])
-# print('Last update:          ', RESULT[5])
-# print('Outbound Interface:   ', RESULT[6])
-# print()
-# f.close()
+f = open('7.1.txt')
+ospf_route = f.read()
+ospf_route = ospf_route.replace('O','OSPF').replace('[', '').replace(']', '').replace(',', '')
+RESULT = ospf_route.split(' ')
+print('Protocol:             ', RESULT[0])
+print('Prefix:               ', RESULT[1])
+print('AD/Metric:            ', RESULT[2])
+print('Next-Hop:             ', RESULT[4])
+print('Last update:          ', RESULT[5])
+print('Outbound Interface:   ', RESULT[6])
+print()
+f.close()
 
 # ignore = ['duplex', 'alias', 'Current configuration']
 # config_sw1 = open(argv[1] + '.txt')
@@ -25,10 +25,20 @@
 #         cleared.write(line)
 # cleared.close()
 
-CAM_table = open('7.3.txt')
-Cam = CAM_table.read()
-CAM_table.seek(Cam.find('Ports')+f.readline())
-for line in CAM_table:
-    print(line.rstrip())
-    result = line.rstrip().split()
-print(result)
+phrase='10'
+a = []
+with open('7.3.txt','r') as f:
+    for (i, line) in enumerate(f):
+        b = line.split(maxsplit=1)
+        if len(b) != 0 and argv[1] == b[0]:
+            a += line.rstrip().replace('  DYNAMIC  ','').split('\n')
+if len(a)-1 >= 0:
+    for i in range(len(a)-1):
+        if a[i].split()[0] > a[i+1].split()[0]:
+            c = a[i]
+            a[i] = a[i+1]
+            a[i+1] = c
+        print(a[i])
+    print(a[len(a)-1])
+else:
+    print('Не найдено')
