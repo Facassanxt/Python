67be8af5d362f5bce7b0a04e8dfc51ed2516f91f
Алексей Стебло <asteblo@gmail.com>
2019-09-23 17:43:52 +0300

    fix

--- 15/task_15_1.py
+++ 15/task_15_1.py
@@ -3,13 +3,10 @@ def get_ip_from_cfg(file_name):
     with open('15\\' + file_name + '.txt','r') as f:
         list = []
         for line in f:
-            match  = re.search(r'ip address (?P<ip>([0-9]{1,3}[\.]){3}[0-9]{1,3}) (?P<mask>([0-9]{1,3}[\.]){3}[0-9]{1,3})', line)
+            match  = re.search(r'ip address (\S+) (\S+)', line)
             if match != None:
-                list_ip = match.group('ip')
-                list_mask = match.group('mask')
-                tuple = (list_ip,list_mask)
-                list.append(tuple)
+                list.append(match.groups())
     return list
-    
+
 if __name__ == "__main__":
     print(get_ip_from_cfg('config_r1'))
